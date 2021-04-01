2f9f660af06e0d9773e730813947fc86934ec4b8
Luiz Fernando Verissimo <luizfverissimo@gmail.com>
2021-04-01 15:40:14 -0300

    data ADDED

---
+++ pages/cthulhu-ep4.js
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+
+import Background from '../components/Background';
+import Content from '../components/Content';
+import Navbar from '../components/Navbar';
+import data from '../data.json'
+
+
+export default function Home() {
+  return (
+    <div className='flex flex-col items-center justify-start min-h-screen'>
+      <Head>
+        <title>NerdCast RPG Cthulhu</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Background type={data.cthulhu[3].type} />
+
+      <Navbar />
+      <main className='w-full flex-1 max-w-7xl flex flex-col items-center justify-center z-0'>
+        <Content data={data.cthulhu[3]}/>
+      </main>
+    </div>
+  );
+}
